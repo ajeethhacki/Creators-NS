21324424a71679973d134aae31745c8906ee1036
msgtovp <msgtovp@gmail.com>
2016-09-16 20:49:51 +0530

    Update Lesson-002.js

--- Lesson-002.js
+++ Lesson-002.js
@@ -20,3 +20,22 @@ var test = function() {
   console.log('Hello'); 
 }
 test()    // Calling
+
+
+function sumOfArray(arr) {
+    if (arr.length <= 0) {
+        console.log("Sum will be 0");
+    } else {
+        sum = 0;
+        for (var i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        console.log("Sum is " + sum);
+    }
+}
+
+sumOfArray([])  // Sum is 0
+
+sumOfArray([1,2,3,4])  // Sum is 10
+
+
