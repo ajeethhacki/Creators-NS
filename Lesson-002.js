3823bfc561b7fd013a1de64bc6247d9ee7acb492
msgtovp <msgtovp@gmail.com>
2016-09-16 20:44:28 +0530

    Update Lesson-002.js

--- Lesson-002.js
+++ Lesson-002.js
@@ -1,1 +1,22 @@
-var test = function testMethod() { console.log('Hello'); }
+function testFunc() { 
+  console.log("Hello.. "); 
+  return 1; 
+}
+testFunc()    // Calling
+
+function testFunc(user) { 
+  console.log("Hello.. "+user); 
+  return 1; 
+}
+testFunc('Steve')    // Calling
+
+var test = function testMethod() { 
+  console.log('Hello'); 
+}
+test()    // Calling
+
+
+var test = function() { 
+  console.log('Hello'); 
+}
+test()    // Calling
